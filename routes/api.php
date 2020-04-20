cd9fb24939fa2933e3f1bd31a4d33ccac69e3582
Jose Airton Feitoza <airton.feitoza@midtec.net.br>
2020-04-20 01:13:19 -0300

    Rotas

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function () {
+    Route::prefix('user')->name('user.')->group(function () {
+        Route::get('/', 'UserController@index')->name('user');
+    });
+});
